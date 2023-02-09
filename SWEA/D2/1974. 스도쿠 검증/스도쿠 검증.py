cb74d8df3eade20537c4967046fd32f8d768ce1c
SungWoo Lee <122414591+seanwoori@users.noreply.github.com>
2023-02-09 15:25:31 +0900

    [D2] Title: 스도쿠 검증, Time: 156 ms, Memory: 58,264 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,28 +1,15 @@
-#import sys
-#sys.stdin = open("input.txt", "r")
-
 T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
-    # ///////////////////////////////////////////////////////////////////////////////////
     mat_lst = []
-    '''
-    if test_case == 1:
-        pass
-    else:
-        continue
-    '''
 
     for i in range(9):
         mat_lst.append(list(map(int, input().split())))
-    #print(mat_lst)
 
     def factorial(n):
         result = 1
         for i in range(1, 1+n):
             result *= i
         return result
-
     
     def row(mat):
         flag = True
@@ -50,46 +37,23 @@ for test_case in range(1, T + 1):
                 break
         return flag
 
-
-    #print(mat_lst)
-
     def nine(mat):
-        flag = True
-        temp_mat = []
-        for_loop = True
-
-        for m in range(3):
+        for _ in range(3):
             temp_mat = []
             for i in range(3):                
-            
-                temp_mat.append(mat.pop(0))        
-            
+                temp_mat.append(mat.pop(0))                
                 if i == 2 or i == 5 or i == 8:
-                    for j in range(3):
+                    for _ in range(3):
                         cert = 1
                         for k in range(3):
-                            for l in range(3):
+                            for _ in range(3):
                                 cert *= temp_mat[k].pop(0)
-                        #print(cert)
-                        #print(temp_mat)
                         if cert == factorial(9):
-                            flag = True
+                            return True
                         else:
-                            flag = False
-                            for_loop = False
-                            break
-                    if for_loop == False:
-                        break
-                if for_loop == False:
-                    break
-            if for_loop == False:
-                break
-        return flag
-                         
+                            return False                       
     flag_ovr = 0
-    
     if (row(mat_lst) == True) and (col(mat_lst) == True) and (nine(mat_lst) == True):
         flag_ovr = 1
 
     print(f'#{test_case} {flag_ovr}')
-    # ///////////////////////////////////////////////////////////////////////////////////
