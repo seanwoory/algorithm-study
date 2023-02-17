6061da5b563d41a86a7705bf269d7bf3d99fe5ce
SungWoo Lee <122414591+seanwoori@users.noreply.github.com>
2023-02-17 09:44:54 +0900

    [D4] Title: 쇠막대기 자르기, Time: 215 ms, Memory: 62,492 KB -BaekjoonHub

---
+++ SWEA/D4/5432. 쇠막대기 자르기/쇠막대기 자르기.py
@@ -0,0 +1,15 @@
+T = int(input())
+for tc in range(1, 1+T):
+    st = input()
+    ans=cnt=0
+    for i in range(len(st)):
+        if st[i]=='(':
+            cnt+=1
+        else: # ')' 바로 앞의 기호를 확인해야함.
+            if st[i-1]=='(': # 레이저의 경우,
+                cnt-=1
+                ans+=cnt
+            else:
+                cnt-=1
+                ans+=1
+    print(f'#{tc} {ans}')
