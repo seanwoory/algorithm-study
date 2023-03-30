03c349662ff889ec0a4b41e86a4011ed56cc9fc5
SungWoo Lee <122414591+seanwoori@users.noreply.github.com>
2023-03-30 17:33:52 +0900

    [Gold IV] Title: N-Queen, Time: 6580 ms, Memory: 124972 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,19 @@
+def dfs(n):
+    global ans
+    if n==N:
+        ans+=1
+        return
+
+    for j in range(N):
+        if v1[j]==v2[n+j]==v3[n-j]==0:
+            v1[j]=v2[n+j]=v3[n-j]=1
+            dfs(n+1)
+            v1[j] = v2[n+j] = v3[n-j] = 0
+
+
+N=int(input())
+arr = [[0]*N for _ in range(N)]
+ans = 0
+v1, v2, v3=[[0]*(2*N) for _ in range(3)]
+dfs(0)
+print(ans)
